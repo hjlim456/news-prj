5e3249cb4d13917f85e821d10348880bd1645540
hyojun <hjlim45678@gmail.com>
2024-02-20 19:12:01 +0900

    https에 http로 되게끔 재설정

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ const API_KEY = "72f346a1d9624ee8b8e4625ec3216c7e"
 const getLatestNews = async()=>{
 
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=조선`)
+    const url = new URL(`https://hj-news.netlify.app/top-headlines`)
 
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data)
     let news = data.articles;
     console.log("nnn",news)
-
+ 
 
 }
 getLatestNews()
