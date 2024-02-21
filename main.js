9b9a237235f3aef8a6664ae18bd1c5b9f37ced72
hyojun <hjlim45678@gmail.com>
2024-02-21 14:48:20 +0900

    화요일과제 테스트

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const API_KEY = "72f346a1d9624ee8b8e4625ec3216c7e"
 const getLatestNews = async()=>{
 
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
     const url = new URL(`https://hj-news.netlify.app/top-headlines`)
 
     const response = await fetch(url)
