51124b5093a088689cf3d4f9a8817345f0478107
hyojun <hjlim45678@gmail.com>
2024-02-21 18:38:11 +0900

    수정

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let newsList = [];
 const getLatestNews = async()=>{
 
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
-    // const url = new URL(`https://hj-news.netlify.app/top-headlines`)
+    // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
+    const url = new URL(`https://hj-news.netlify.app/top-headlines`)
 
     const response = await fetch(url)
     const data = await response.json()
